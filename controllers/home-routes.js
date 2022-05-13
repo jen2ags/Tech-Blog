4aeb0f4de90fee32ff24a50343e7e55b56131cdb
jennj <jennjenn93@gmail.com>
2022-05-13 14:14:51 -0500

    homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,38 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
-router.get('/', (req,res) => {
-    res.render('homepage',{
-        id: 1,
-        title: 'Cool Kicks',
-        blog_text: 'Look at these awesome new cases for your tech!',
-        created_at: new Date(),
-        comments: [{}, {}],
-        user: {
-            username: 'tech_junkie'
-        }
-    });
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'blog_text',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['user']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json;
+        });
 });
 
 module.exports = router;
